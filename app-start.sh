bb556371abf76f10c6fed7b74eebad51c7b97cfe
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 17:29:03 +0100

    Shout harder

--- app-start.sh
+++ app-start.sh
@@ -3,6 +3,8 @@
 echo "Starting application"
 echo "This is a test" > /home/ec2-user/TwitterWallApp/test.log
 source /home/ec2-user/TwitterWallConfig/env.sh
+echo "This is a also test" > /home/ec2-user/TwitterWallApp/test2.log
 sudo -E npm start > logs/out.log 2> logs/err.log < /dev/null &
+echo "This is the last test" > /home/ec2-user/TwitterWallApp/test3.log
 echo "Application started successfully!"
 
