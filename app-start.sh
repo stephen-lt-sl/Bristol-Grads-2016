77db5ffd57530f69b9360b53f78688e8e2bffedb
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 17:24:29 +0100

    Suicide

--- app-start.sh
+++ app-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting application"
+kill
 source /home/ec2-user/TwitterWallConfig/env.sh
 sudo -E npm start > logs/out.log 2> logs/err.log < /dev/null &
 echo "Application started successfully!"
