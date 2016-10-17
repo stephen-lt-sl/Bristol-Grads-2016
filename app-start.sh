e125861ff8bfea95d44f0efdeb98c1f124215024
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 18:21:11 +0100

    Stay on target

--- app-start.sh
+++ app-start.sh
@@ -3,8 +3,9 @@
 set -e
 
 echo "Starting application"
-source /home/ec2-user/TwitterWallConfig/env.sh > /home/ec2-user/TwitterWallApp/logs/src.log
-npm start > logs/out.log 2> logs/err.log < /dev/null
-disown -h `pgrep node` > /home/ec2-user/TwitterWallApp/logs/dis.log
+logsFolder=/home/ec2-user/TwitterWallApp/logs
+source /home/ec2-user/TwitterWallConfig/env.sh > $logsFolder/src.log
+npm start > $logsFolder/out.log 2> $logsFolder/err.log < /dev/null
+disown -h `pgrep node` > $logsFolder/dis.log
 echo "Application started successfully!"
 
