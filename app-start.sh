3c561f666db953ee957197cf3f0b356d3800f9b6
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 17:26:55 +0100

    Shout out

--- app-start.sh
+++ app-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting application"
-kill
+echo "This is a test" > /home/ec2-user/TwitterWallApp/test.log
 source /home/ec2-user/TwitterWallConfig/env.sh
 sudo -E npm start > logs/out.log 2> logs/err.log < /dev/null &
 echo "Application started successfully!"
