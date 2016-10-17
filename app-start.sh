41ad16447c94ebd1f60ae53b7abc5c2956f09907
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 17:41:27 +0100

    Syntax it

--- app-start.sh
+++ app-start.sh
@@ -3,6 +3,6 @@
 echo "Starting application"
 source /home/ec2-user/TwitterWallConfig/env.sh
 sudo -E npm start > logs/out.log 2> logs/err.log < /dev/null &
-pgrep node | sudo xargs disown
+disown `pgrep node`
 echo "Application started successfully!"
 
