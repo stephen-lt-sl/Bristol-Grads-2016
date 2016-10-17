765278b297165310db927d42ffa97665e0196c6f
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 17:44:18 +0100

    More suicide

--- app-start.sh
+++ app-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting application"
+exit 1
 source /home/ec2-user/TwitterWallConfig/env.sh
 sudo -E npm start > logs/out.log 2> logs/err.log < /dev/null &
 disown `pgrep node`
