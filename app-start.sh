6f10fcd2f5ab00c30d33798ff90dd41c53e97f1c
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-17 17:49:27 +0100

    Lose authority

--- app-start.sh
+++ app-start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "Starting application"
-exit 1
 source /home/ec2-user/TwitterWallConfig/env.sh
-sudo -E npm start > logs/out.log 2> logs/err.log < /dev/null &
+npm start > logs/out.log 2> logs/err.log < /dev/null &
 disown `pgrep node`
 echo "Application started successfully!"
 
