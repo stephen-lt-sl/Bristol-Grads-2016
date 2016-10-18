449126514e136f735efd50b736bc900587150a0a
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-18 09:11:05 +0100

    Make a place to stay

--- app-config.sh
+++ app-config.sh
@@ -18,13 +18,13 @@ sudo env NODE_ENV=production grunt build
 
 # Copy server config files stored in the EC2 directory to the appropriate folder
 echo "Fetching configuration files..."
-sudo mkdir /home/ec2-user/TwitterWallApp/server/config
+sudo mkdir -p /home/ec2-user/TwitterWallApp/server/config
 sudo cp /home/ec2-user/TwitterWallConfig/adminConfig.json /home/ec2-user/TwitterWallApp/server/config/adminConfig.json
 sudo cp /home/ec2-user/TwitterWallConfig/eventConfig.json /home/ec2-user/TwitterWallApp/server/config/eventConfig.json
 
 echo "Creating log folder..."
 sudo rm -r /home/ec2-user/TwitterWallApp/logs
-sudo mkdir /home/ec2-user/TwitterWallApp/logs
+sudo mkdir -p /home/ec2-user/TwitterWallApp/logs
 
 echo "Install complete."
 
