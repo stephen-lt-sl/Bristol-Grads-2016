795665478d7d4affbda802ebdf5c6f612dfd08e3
Stephen Tozer <stozer@scottlogic.co.uk>
2016-10-18 09:00:41 +0100

    Make up your mind

--- app-config.sh
+++ app-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 sudo yum -y update
 
 echo "Installing nodejs to yum..."
--- app-start.sh
+++ app-start.sh
@@ -5,7 +5,7 @@ set -e
 echo "Starting application"
 logsFolder=/home/ec2-user/TwitterWallApp/logs
 source /home/ec2-user/TwitterWallConfig/env.sh > $logsFolder/src.log
-npm start > $logsFolder/out.log 2> $logsFolder/err.log < /dev/null
+cd /home/ec2-user/TwitterWallApp && npm start > $logsFolder/out.log 2> $logsFolder/err.log < /dev/null
 disown -h `pgrep node` > $logsFolder/dis.log
 echo "Application started successfully!"
 
